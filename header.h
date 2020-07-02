06bd147a8670b5eb5da4792eb0fada631d32f445
Nikita-ad <67658542+Nikita-ad@users.noreply.github.com>
2020-07-02 13:40:12 +0300

    Update header.h

--- header.h
+++ header.h
@@ -31,55 +31,66 @@ private:
     ///кол-во ячеек для 1 кровати на складе
     int bed_cost = 5;
     ///кол-во кроватей на складе
-    int bed_count = 0;
+    int* bed_count;
 
     int cabinet_doska_cost = 7;
     int cabinet_brusok_cost = 3;
     int cabinet_cost = 8;
-    int cabinet_count = 0;
+    int* cabinet_count;
 
     int table_doska_cost = 4;
     int table_brusok_cost = 4;
     int table_cost = 3;
-    int table_count = 0;
+    int* table_count;
 
     ///кол-во ячеек на складе
-    int storage_capacity = 100;
+    int* storage_capacity;
     ///кол-во заполненных ячеек
-    int curr_capacity = 0;
+    int* curr_capacity;
+    ///кол-во складов
+    int storage_count = 0;
 
 public:
-    int get_bed_count();
+    int get_bed_count(int index);
     int get_bed_doska_cost();
     int get_bed_brusok_cost();
     int get_bed_cost();
-    void set_bed_count(int val);
+    void set_bed_count(int index, int val);
     void set_bed_doska_cost(int val);
     void set_bed_brusok_cost(int val);
     void set_bed_cost(int val);
 
-    int get_cabinet_count();
+    int get_cabinet_count(int index);
     int get_cabinet_doska_cost();
     int get_cabinet_brusok_cost();
     int get_cabinet_cost();
-    void set_cabinet_count(int val);
+    void set_cabinet_count(int index, int val);
     void set_cabinet_doska_cost(int val);
     void set_cabinet_brusok_cost(int val);
     void set_cabinet_cost(int val);
 
-    int get_table_count();
+    int get_table_count(int index);
     int get_table_doska_cost();
     int get_table_brusok_cost();
     int get_table_cost();
-    void set_table_count(int val);
+    void set_table_count(int index, int val);
     void set_table_doska_cost(int val);
     void set_table_brusok_cost(int val);
     void set_table_cost(int val);
 
-    int get_storage_capacity();
-    int get_curr_capacity();
-    void set_storage_capacity(int val);
-    void set_curr_capacity(int val);
+    int get_storage_capacity(int index);
+    int get_curr_capacity(int index);
+    void set_storage_capacity(int index, int val);
+    void set_curr_capacity(int index, int val);
+
+    int get_storage_count();
+    void set_storage_count(int val);
+
+    void init_bed_count();
+    void init_cabinet_count();
+    void init_table_count();
+    void init_storage_capacity();
+    void init_curr_capacity();
 
     friend std::ostream& operator<<(std::ostream &out, const material a);
     friend std::ostream& operator<<(std::ostream &out, const furniture a);
@@ -119,13 +130,12 @@ std::ofstream get_outf(char* filename);
 
 
 
-
 /**
 @function craft_bed
 В соответствии с введенным кол-вом кроватей, которые нужно создать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 и material(кол-во брусков, досок)
 */
-void craft_bed(furniture* a, material* b);
+void craft_bed(furniture* a, material* b, int index);
 
 
 
@@ -135,7 +145,7 @@ void craft_bed(furniture* a, material* b);
 В соответствии с введенным кол-вом шкафов, которые нужно создать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 и material(кол-во брусков, досок)
 */
-void craft_cabinet(furniture* a, material* b);
+void craft_cabinet(furniture* a, material* b, int index);
 
 
 
@@ -145,7 +155,7 @@ void craft_cabinet(furniture* a, material* b);
 В соответствии с введенным кол-вом столов, которые нужно создать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 и material(кол-во брусков, досок)
 */
-void craft_table(furniture* a, material* b);
+void craft_table(furniture* a, material* b, int index);
 
 
 
@@ -154,7 +164,7 @@ void craft_table(furniture* a, material* b);
 @function craft
 Функция для создания мебели
 */
-void craft(char* furniture_filename, char* material_filename);
+void craft(char* furniture_filename, char* material_filename, char* storage_filename);
 
 
 
@@ -163,7 +173,7 @@ void craft(char* furniture_filename, char* material_filename);
 @function sell_bed
 В соответствии с введенным кол-вом кроватей, которые нужно продать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 */
-void sell_bed(furniture* a);
+void sell_bed(furniture* a, int index);
 
 
 
@@ -172,7 +182,7 @@ void sell_bed(furniture* a);
 @function sell_cabinet
 В соответствии с введенным кол-вом шкафов, которые нужно продать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 */
-void sell_cabinet(furniture* a);
+void sell_cabinet(furniture* a, int index);
 
 
 
@@ -181,7 +191,7 @@ void sell_cabinet(furniture* a);
 @function sell_table
 В соответствии с введенным кол-вом столов, которые нужно продать изменяются поля в классах furniture(кол-во данного типа мебели, текущая заполненность на складе)
 */
-void sell_table(furniture* a);
+void sell_table(furniture* a, int index);
 
 
 
@@ -190,7 +200,7 @@ void sell_table(furniture* a);
 @function sell
 Функция для продажи мебели
 */
-void sell(char* furniture_filename);
+void sell(char* furniture_filename, char* storage_filename);
 
 
 
@@ -199,7 +209,7 @@ void sell(char* furniture_filename);
 @function show_bed_info
 Выводит информацию о кроватях на складе
 */
-void show_bed_info(furniture* a);
+void show_bed_info(furniture* a, int index);
 
 
 
@@ -208,7 +218,7 @@ void show_bed_info(furniture* a);
 @function show_cabinet_info
 Выводит информацию о шкафах на складе
 */
-void show_cabinet_info(furniture* a);
+void show_cabinet_info(furniture* a, int index);
 
 
 
@@ -217,7 +227,7 @@ void show_cabinet_info(furniture* a);
 @function show_table_info
 Выводит информацию о столах на складе
 */
-void show_table_info(furniture* a);
+void show_table_info(furniture* a, int index);
 
 
 
@@ -226,7 +236,7 @@ void show_table_info(furniture* a);
 @function _find
 В зависимости от типа мебели, показывает информацию о нем через функции show
 */
-void _find(char* furniture_filename);
+void _find(char* furniture_filename, char* storage_filename);
 
 
 
@@ -236,3 +246,30 @@ void _find(char* furniture_filename);
 Показывает информацию о материалах (кол-во брусков и досок)
 */
 void show_material_info(char* material_filename);
+
+
+
+
+/**
+@function dump_furniture
+Загружает мебель в файл, элементы динамичных полей в отдельный файл
+*/
+void dump_furniture(furniture a, char* furniture_filename, char* storage_filename);
+
+
+
+
+/**
+@function load_furniture
+Загружает мебель из файла, элементы динамичных полей из отдельного файла
+*/
+void load_furniture(furniture* a, char* storage_filename);
+
+
+
+
+/**
+@function add_storage
+Добавляет склад
+*/
+void add_storage(char* furniture_filename, char* storage_filename);
